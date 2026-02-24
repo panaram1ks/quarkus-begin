95780908ab860c4ed4bb1d037f2db7b38e315d1d
parom <E.Parominskiy@isida.by>
2026-02-24 13:51:14 +0300

    Activity: Injection Bean

---
+++ src/main/java/org/agoncal/quarkus/starting/BookRepository.java
@@ -0,0 +1,25 @@
+package org.agoncal.quarkus.starting;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.PathParam;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class BookRepository {
+
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1, "Me","Title1", 2025, "IT"),
+                new Book(2, "Him","Title2", 2023, "IT"),
+                new Book(3, "She","Title3", 2019, "IT")
+        );
+    }
+
+    public Optional<Book> getBook(int id){
+        return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    }
+
+
+}
--- src/main/java/org/agoncal/quarkus/starting/BookResource.java
+++ src/main/java/org/agoncal/quarkus/starting/BookResource.java
@@ -1,38 +1,45 @@
 package org.agoncal.quarkus.starting;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 import java.util.Optional;
+
 
 @Path("/api/books")
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
+    @Inject
+    private BookRepository bookRepository;
+    @Inject
+    private Logger logger;
+
     @GET
     public List<Book> getAllBooks() {
-        return List.of(
-                new Book(1, "Me","Title1", 2025, "IT"),
-                new Book(2, "Him","Title2", 2023, "IT"),
-                new Book(3, "She","Title3", 2019, "IT")
-        );
+        logger.log(Logger.Level.INFO, "getAllBooks()");
+        return bookRepository.getAllBooks();
     }
 
     @GET
     @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    public int countAllBooks(){
+    public int countAllBooks() {
+        logger.log(Logger.Level.INFO, "countAllBooks()");
         return getAllBooks().size();
     }
 
     @GET
     @Path("{id}")
-    public Optional<Book> getBook(@PathParam("id") int id){
-       return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        logger.log(Logger.Level.INFO, "getBook(int id)");
+        return bookRepository.getBook(id);
     }
 
 }
