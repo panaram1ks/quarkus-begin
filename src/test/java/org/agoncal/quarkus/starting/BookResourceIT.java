3401d458a8d797f993638f10ca19ca0d50a846c9
parom <E.Parominskiy@isida.by>
2026-02-25 08:42:58 +0300

    Packaging the Application Activity: Testing Native Executables

--- src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceIT.java
@@ -1,9 +1,29 @@
 package org.agoncal.quarkus.starting;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static jakarta.ws.rs.core.HttpHeaders.ACCEPT;
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.OK;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
 class BookResourceIT extends BookResourceTest {
 
-
+    // we able override test from jvm mode to native mode
+    @Test
+    void shouldGetABook() {
+        given()
+                .header(ACCEPT, APPLICATION_JSON)
+                .pathParam("id", 1)
+                .when()
+                .get("/api/books/{id}")
+                .then()
+                .statusCode(OK.getStatusCode())
+                .body("title", is("Title1"))
+                .body("yearOfPublication", is(2025))
+                .body("genre", is("IT"));
+    }
 }
